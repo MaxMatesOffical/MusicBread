9288dc478ef0574bb7fa86a5b0f8c675568dad9b
MaxMatesOffical <80164267+MaxMatesOffical@users.noreply.github.com>
2021-03-07 14:45:05 +0100

    Create github.js

---
+++ commands/github.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'github',
+    description: "this is a github command!",
+    execute(message, args){
+        message.channel.send('This is the offical github for Music bread: https://github.com/MaxMatesOffical/MusicBread');
+    }
+}
